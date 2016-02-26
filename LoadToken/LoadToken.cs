ee28fc43d4a0cb36f3ca56523b39976fa8207560
Licshee <licshee@yen.ai>
2016-02-26 17:41:22 +0800

    prepare for properties

--- LoadToken/LoadToken.cs
+++ LoadToken/LoadToken.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 public static partial class LoadToken
 {
+    public const BindingFlags AllMembers = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+
     public static MethodInfo ForMethod(Delegate method)
-    {
-#if LoadTokenEx
-        throw new NotImplementedException();
-#else
-        return method.Method;
-#endif
-    }
+        => method.Method;
+
+    public static PropertyInfo ForPropertyGetter(MethodInfo method)
+        =>  method.DeclaringType.GetProperties(AllMembers).Single(p => p.GetGetMethod(true) == method);
+    public static PropertyInfo ForPropertySetter(MethodInfo method)
+        =>  method.DeclaringType.GetProperties(AllMembers).Single(p => p.GetSetMethod(true) == method);
+
+    public static PropertyInfo ForProperty<T>(Func<T> getter)
+        =>  ForPropertyGetter(getter.Method);
+    public static PropertyInfo ForProperty<T>(Action<T> setter)
+        =>  ForPropertySetter(setter.Method);
 }
