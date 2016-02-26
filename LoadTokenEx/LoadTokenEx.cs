535c87f5b84f813bbad29194c23acf943e21b4dd
Licshee <licshee@yen.ai>
2016-02-26 17:15:13 +0800

    prepare for Fody add-in

---
+++ LoadTokenEx/LoadTokenEx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+public static partial class LoadToken
+{
+    public static FieldInfo ForField<T>(ref T reference)
+    {
+        throw new NotImplementedException();
+    }
+}
