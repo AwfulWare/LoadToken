0fb075e4646de68313a05c833b1c5f9f352d8f8d
Licshee <licshee@yen.ai>
2016-02-26 17:10:10 +0800

    1.0

---
+++ LoadToken/LoadToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+public static partial class LoadToken
+{
+    public static MethodInfo ForMethod(Delegate method)
+    {
+#if LoadTokenEx
+        throw new NotImplementedException();
+#else
+        return method.Method;
+#endif
+    }
+}
